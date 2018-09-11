328c881997f348e3c8296185e79e85b4276dcadd
Zengyijie <jackiezeng01@163.com>
2018-09-11 23:22:28 +0800

    修复sh脚本文件

--- update_Snippets.sh
+++ update_Snippets.sh
@@ -1,8 +1,8 @@
 # 移动、修改该文件（假如存在的话）
-# mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
 # 先移除原来的文件
-rm ~/Library/Developer/Xcode/UserData/CodeSnippets
+#rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
 #pwd 代表当前路径
 #ln 命令是链接两个文件 如：ln -s file1 file2 (即将这两个文件保持一致性)
